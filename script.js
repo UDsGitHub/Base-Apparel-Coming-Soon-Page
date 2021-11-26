81b301bc9dd331e22eb4844f2913a742b0fcc630
UD <dingding8003@gmail.com>
2021-11-26 01:20:30 -0600

    nothing serious

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ form.addEventListener('submit', (e)=>{
         errorTxt.style.display = "none";
     }
 })
+
 function validateEmail(email) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
